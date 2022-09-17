6b8887f649514bf0712e6af396030c30985901a3
Nick Lu <tlu32@jhu.edu>
2022-09-17 00:00:20 -0400

    update

--- image_process/scan.py
+++ image_process/scan.py
@@ -43,14 +43,3 @@ def findcoordinates(image):
     # show the contour (outline) of the piece of paper
     print("STEP 2: Find contours of paper")
     return -1
-
-    # # apply the four point transform to obtain a top-down
-    # # view of the original image
-    # warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
-    # # convert the warped image to grayscale, then threshold it
-    # # to give it that 'black and white' paper effect
-    # warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-    # T = threshold_local(warped, 11, offset=10, method="gaussian")
-    # warped = (warped > T).astype("uint8") * 255
-    # # show the original and scanned images
-    # print("STEP 3: Apply perspective transform")
