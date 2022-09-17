f8731a753a4874418680ba867a1b3265a1dbcb93
Nick Lu <tlu32@jhu.edu>
2022-09-17 14:31:39 -0400

    combine into one

--- human_detection/detect.py
+++ human_detection/detect.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # Carregar modelos
 detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
-labels = pd.read_csv('labels.csv',sep=';',index_col='ID')
+labels = pd.read_csv('labels.csv', sep=';', index_col='ID')
 labels = labels['OBJECT (2017 REL.)']
 
 cap = cv2.VideoCapture(0)
@@ -14,48 +14,48 @@ cap = cv2.VideoCapture(0)
 width = 512
 height = 512
 
-while(True):
-    #Capture frame-by-frame
+while (True):
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    
-    #Resize to respect the input_shape
-    inp = cv2.resize(frame, (width , height ))
 
-    #Convert img to RGB
+    # Resize to respect the input_shape
+    inp = cv2.resize(frame, (width, height))
+
+    # Convert img to RGB
     rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
 
-    #Is optional but i recommend (float convertion and convert img to tensor image)
+    # Is optional but i recommend (float convertion and convert img to tensor image)
     rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
 
-    #Add dims to rgb_tensor
-    rgb_tensor = tf.expand_dims(rgb_tensor , 0)
-    
+    # Add dims to rgb_tensor
+    rgb_tensor = tf.expand_dims(rgb_tensor, 0)
+
     boxes, scores, classes, num_detections = detector(rgb_tensor)
-    
+
     pred_labels = classes.numpy().astype('int')[0]
-    
+
     pred_labels = [labels[i] for i in pred_labels]
     pred_boxes = boxes.numpy()[0].astype('int')
     pred_scores = scores.numpy()[0]
-    #loop throughout the faces detected and place a box around it
-    
+    # loop throughout the faces detected and place a box around it
+
     img_boxes = []
-    
-    for score, (ymin,xmin,ymax,xmax), label in zip(pred_scores, pred_boxes, pred_labels):
+
+    for score, (ymin, xmin, ymax, xmax), label in zip(pred_scores, pred_boxes, pred_labels):
         if score < 0.5:
             continue
-            
-        score_txt = f'{100 * round(score,0)}'
-        img_boxes = cv2.rectangle(rgb,(xmin, ymax),(xmax, ymin),(0,255,0),1)      
+
+        score_txt = f'{100 * round(score, 0)}'
+        img_boxes = cv2.rectangle(rgb, (xmin, ymax), (xmax, ymin), (0, 255, 0), 1)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img_boxes,label,(xmin, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
-        cv2.putText(img_boxes,score_txt,(xmax, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
+        cv2.putText(img_boxes, label, (xmin, ymax - 10), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(img_boxes, score_txt, (xmax, ymax - 10), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
 
     if len(img_boxes) == 0:
         continue
-    
-    #Display the resulting frame
-    cv2.imshow('Result',img_boxes)
+
+    # Display the resulting frame
+    cv2.imshow('Result', img_boxes)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- image_process/formatImg.py
+++ image_process/formatImg.py
@@ -1,5 +1,3 @@
-from transform import order_points
-from scan import findcoordinates
 from skimage.filters import threshold_local
 import math
 import cv2
@@ -7,83 +5,182 @@ import imutils
 import scipy.spatial.distance
 import numpy as np
 
-img = cv2.imread('images/img1.jpg')
-(rows, cols, _) = img.shape
 
-# image center
-u0 = cols / 2.0
-v0 = rows / 2.0
+def outputNote(img):
+    (rows, cols, _) = img.shape
 
-# detected corners on the original image
-p = order_points(findcoordinates(img))
-print(p)
+    # image center
+    u0 = cols / 2.0
+    v0 = rows / 2.0
 
-# widths and heights of the projected image
-w1 = scipy.spatial.distance.euclidean(p[0], p[1])
-w2 = scipy.spatial.distance.euclidean(p[2], p[3])
+    # detected corners on the original image
+    p = order_points(findcoordinates(img))
+    print(p)
 
-h1 = scipy.spatial.distance.euclidean(p[0], p[2])
-h2 = scipy.spatial.distance.euclidean(p[1], p[3])
+    # widths and heights of the projected image
+    w1 = scipy.spatial.distance.euclidean(p[0], p[1])
+    w2 = scipy.spatial.distance.euclidean(p[2], p[3])
 
-w = max(w1, w2)
-h = max(h1, h2)
+    h1 = scipy.spatial.distance.euclidean(p[0], p[2])
+    h2 = scipy.spatial.distance.euclidean(p[1], p[3])
 
-# visible aspect ratio
-ar_vis = float(w) / float(h)
+    w = max(w1, w2)
+    h = max(h1, h2)
 
-# make numpy arrays and append 1 for linear algebra
-m1 = np.array((p[0][0], p[0][1], 1)).astype('float32')
-m2 = np.array((p[1][0], p[1][1], 1)).astype('float32')
-m3 = np.array((p[2][0], p[2][1], 1)).astype('float32')
-m4 = np.array((p[3][0], p[3][1], 1)).astype('float32')
+    # visible aspect ratio
+    ar_vis = float(w) / float(h)
 
-# calculate the focal distance
-k2 = np.dot(np.cross(m1, m4), m3) / np.dot(np.cross(m2, m4), m3)
-k3 = np.dot(np.cross(m1, m4), m2) / np.dot(np.cross(m3, m4), m2)
+    # make numpy arrays and append 1 for linear algebra
+    m1 = np.array((p[0][0], p[0][1], 1)).astype('float32')
+    m2 = np.array((p[1][0], p[1][1], 1)).astype('float32')
+    m3 = np.array((p[2][0], p[2][1], 1)).astype('float32')
+    m4 = np.array((p[3][0], p[3][1], 1)).astype('float32')
 
-n2 = k2 * m2 - m1
-n3 = k3 * m3 - m1
+    # calculate the focal distance
+    k2 = np.dot(np.cross(m1, m4), m3) / np.dot(np.cross(m2, m4), m3)
+    k3 = np.dot(np.cross(m1, m4), m2) / np.dot(np.cross(m3, m4), m2)
 
-n21 = n2[0]
-n22 = n2[1]
-n23 = n2[2]
+    n2 = k2 * m2 - m1
+    n3 = k3 * m3 - m1
 
-n31 = n3[0]
-n32 = n3[1]
-n33 = n3[2]
+    n21 = n2[0]
+    n22 = n2[1]
+    n23 = n2[2]
 
-f = math.sqrt(np.abs((1.0 / (n23 * n33)) * ((n21 * n31 - (n21 * n33 + n23 * n31) * u0 + n23 * n33 * u0 * u0) + (
-        n22 * n32 - (n22 * n33 + n23 * n32) * v0 + n23 * n33 * v0 * v0))))
+    n31 = n3[0]
+    n32 = n3[1]
+    n33 = n3[2]
 
-A = np.array([[f, 0, u0], [0, f, v0], [0, 0, 1]]).astype('float32')
+    f = math.sqrt(np.abs((1.0 / (n23 * n33)) * ((n21 * n31 - (n21 * n33 + n23 * n31) * u0 + n23 * n33 * u0 * u0) + (
+            n22 * n32 - (n22 * n33 + n23 * n32) * v0 + n23 * n33 * v0 * v0))))
 
-At = np.transpose(A)
-Ati = np.linalg.inv(At)
-Ai = np.linalg.inv(A)
+    A = np.array([[f, 0, u0], [0, f, v0], [0, 0, 1]]).astype('float32')
 
-# calculate the real aspect ratio
-ar_real = math.sqrt(np.dot(np.dot(np.dot(n2, Ati), Ai), n2) / np.dot(np.dot(np.dot(n3, Ati), Ai), n3))
+    At = np.transpose(A)
+    Ati = np.linalg.inv(At)
+    Ai = np.linalg.inv(A)
 
-if ar_real < ar_vis:
-    W = int(w)
-    H = int(W / ar_real)
-else:
-    H = int(h)
-    W = int(ar_real * H)
+    # calculate the real aspect ratio
+    ar_real = math.sqrt(np.dot(np.dot(np.dot(n2, Ati), Ai), n2) / np.dot(np.dot(np.dot(n3, Ati), Ai), n3))
 
-pts1 = np.array(p).astype('float32')
-pts2 = np.float32([[0, 0], [W, 0], [0, H], [W, H]])
+    if ar_real < ar_vis:
+        W = int(w)
+        H = int(W / ar_real)
+    else:
+        H = int(h)
+        W = int(ar_real * H)
 
-# project the image with the new w/h
-M = cv2.getPerspectiveTransform(pts1, pts2)
+    pts1 = np.array(p).astype('float32')
+    pts2 = np.float32([[0, 0], [W, 0], [0, H], [W, H]])
 
-dst = cv2.warpPerspective(img, M, (W, H))
+    # project the image with the new w/h
+    M = cv2.getPerspectiveTransform(pts1, pts2)
 
-dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
-T = threshold_local(dst, 11, offset=10, method="gaussian")
-dst = (dst > T).astype("uint8") * 255
-# show the original and scanned images
-print("STEP 3: Apply perspective transform")
-cv2.imshow("Original", img)
-cv2.imshow("Scanned", dst)
-cv2.waitKey(0)
+    dst = cv2.warpPerspective(img, M, (W, H))
+
+    dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+    T = threshold_local(dst, 11, offset=10, method="gaussian")
+    dst = (dst > T).astype("uint8") * 255
+    # show the original and scanned images
+    print("STEP 3: Apply perspective transform")
+    cv2.imshow("Original", img)
+    cv2.imshow("Scanned", dst)
+    cv2.waitKey(0)
+
+
+def findcoordinates(image):
+    # load the image and compute the ratio of the old height
+    # to the new height, clone it, and resize it
+    ratio = image.shape[0] / 500.0
+    image = imutils.resize(image, height=500)
+    # convert the image to grayscale, blur it, and find edges
+    # in the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (9, 9), 0)
+
+    edged = cv2.Canny(gray, 50, 200)
+    # show the original image and the edge detected image
+    print("STEP 1: Edge Detection")
+    cv2.imshow("Image", image)
+    cv2.imshow("Edged", edged)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # find the contours in the edged image, keeping only the
+    # largest ones, and initialize the screen contour
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.05 * peri, True)
+        # if our approximated contour has four points, then we
+        # can assume that we have found our screen
+        cv2.drawContours(image, [approx], -1, (0, 255, 0), 2)
+        cv2.imshow("Outline", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        if len(approx) == 4:
+            screenCnt = approx
+            print(screenCnt.reshape(4, 2) * ratio)
+            return screenCnt.reshape(4, 2) * ratio
+    # show the contour (outline) of the piece of paper
+    print("STEP 2: Find contours of paper")
+    return -1
+
+
+def order_points(pts):
+    # initialize a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[3] = pts[np.argmax(s)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[2] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordinates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    # return the warped image
+    return warped
